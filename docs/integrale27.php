9b0ff99b9d87150972fa63e62efb1d2aeaa1502d
Kai Carver <kaicarver@gmail.com>
2025-12-03 17:09:06 +0100

    page pour saison 27 à jour à tester

--- docs/integrale27.php
+++ docs/integrale27.php
@@ -21,34 +21,106 @@ MM_reloadPage(true);
   <a name="HautPage"></a>
   <p class="annee">Vingt-septième saison 2025-2026</p>
   <img src="../Images/Tribune_614.jpg" width="614" height="176">
-  <p class="liensaisons"><a href="#Octobre">Octobre</a>&nbsp;&nbsp;<a href="#Novembre">Novembre</a> </p>
+  <p class="liensaisons"><a href="#Octobre">Octobre</a>&nbsp;&nbsp;<a href="#Novembre">Novembre</a>&nbsp;&nbsp;<a href="#Decembre">Décembre</a><!--&nbsp;&nbsp;<a href="#Janvier">Janvier</a>&nbsp;&nbsp;<a href="#Fevrier">F&eacute;vrier</a>&nbsp;&nbsp;<a href="#Mars">Mars</a>&nbsp;&nbsp;<a href="#Avril">Avril</a>&nbsp;&nbsp;<a href="#Mai">Mai</a>&nbsp;&nbsp;<a href="#Juin">Juin</a>--></p>
   <hr noshade>
+
   <p class="mois"><a name="Octobre" id="Octobre"></a>OCTOBRE<br>
   <span class="date"> 05/10</span><span class="date">/2025</span>  
   <p align="center" class="bwv">Christian Geist<br>
   Quis hostis in coelis</p>
-<p class="musiciens">&nbsp;</p>
-<p class="musiciens"><br>
-  <br>
- </p>
-<p class="musiciens"><br>
-  *coordination artistique</p>
-<p class="musiciens"><span class="titreorgue">Pi&egrave;ces d'orgue:<br>
-    </span> <span class="pieceorgue"><br>
-  </span><span class="pieceorgue"><a href="#HautPage"><img src="./Images/fleche_haut.gif" alt="Retour haut de page"  border="0" class="flechehaut" /></a></span>
+<p class="musiciens">
+  Ensemble WILHELM VOGEL
+</p>
+<p class="musiciens">
+ Kaoli Isshiki-Didier*, Madeleine Treilhou*, Sophie Decaudaveine, Catherine Joussellin, soprani<br>
+ Brigitte Vinson*, Léa Colmet-Daâge, Angélique Sozza, Elizabeth Cencetti, alti<br>
+ Jean-Luc Baudoin*, Gilles Grimaldi*, Stanislas Herbin, Eliav Lavi, ténors<br>
+ Jean-François Gay*, Eric Martin-Bonnet*, Raphael Willenbrock*, Paul Willenbrock, basses
+</p>
+<p class="musiciens">
+ Emmanuelle Dauvin, Guillaume Humbrecht, Enesh Dzhanykova, violons<br>
+ Jean-Luc Thonnerieux, Cibeles Bullón, Aik-Shin Tan, altos<br>
+ Jerôme Vidaller, violoncelle<br>
+ Valerie Bienvenu, contrebasse<br>
+ Alexandre Salles, basson<br>
+ Victor Theurkauff, Julien Cartier, Antoine Benassy, Hyacinte Ameline, trompettes<br>
+ Ronan Thomas, timbales<br>
+ François Guerrier, clavecin<br>
+ Shotaro Komoto, orgue
+</p>
+<p class="musiciens">
+ Graham O’Reilly, direction
+</p>
+<p class="musiciens">
+  * soliste</p>
+<p class="musiciens"><span class="pieceorgue"><a href="#HautPage"><img src="./Images/fleche_haut.gif" alt="Retour haut de page"  border="0" class="flechehaut" /></a></span>
 <p><span class="mois">Programme du concert :</span> <a href="../Presentations/saison27/2025_10.pdf" target="_blank"><img src="../Images/Acrobat30px.gif" width="30" height="30" border="0"></a>
 </p>
 <hr noshade>
+
 <p class="mois"><a name="Novembre" id="Novembre"></a>NOVEMBRE<br>
   <span class="date"> 02/11</span><span class="date">/2025</span>
 <p align="center" class="bwv">BWV 188<br>
   Ich habe meine Zuversicht</p>
-<p class="musiciens">&nbsp;</p>
-<p class="musiciens"></p>
-<p class="musiciens"></p>
-<p class="musiciens">*coordination artistique</p>
+<p class="musiciens">
+ Marie Théoleyre, soprano<br>
+ Guilhem Terrail, altus<br>
+ Jean-François Novelli, ténor<br>
+ Matthias Seidel, basse
+</p>
+<p class="musiciens">
+ Joseba Berrocal, Margot Humber, hautbois<br>
+ Amadeo Castille, taille de hautbois
+</p>
+<p class="musiciens">
+ Sayaka Shinoda, Colin Heller, Emmanuel Galliot, Aik Shin Tan, violons<br>
+ Cibeles Bullón Muñoz, Ruth Weber, altos<br>
+ Marion Middenway, Camilo Ruiz, violoncelles<br>
+ Michel Frechina, contrebasse<br>
+ Jean-Miguel Aristizabal, clavecin
+</p>
+<p class="musiciens">
+ Freddy Eichelberger, orgue et coordination artistique
+</p>
+<p class="musiciens">
+ Louise Mousset, Sébastien Cadet, Jérémy Neveu, souffleurs
+</p>
 <p class="musiciens"><span class="pieceorgue"><a href="#HautPage"><img src="./Images/fleche_haut.gif" alt="Retour haut de page"  border="0" class="flechehaut" /></a></span>
 <p><span class="mois">Programme du concert :</span> <a href="../Presentations/saison27/2025_11.pdf" target="_blank"><img src="../Images/Acrobat30px.gif" width="30" height="30" border="0"></a> </p>
 <hr noshade="noshade" />
-<hr>
+
+<p class="mois"><a name="Decembre" id="Decembre"></a>DECEMBRE<br>
+  <span class="date"> 01/12</span><span class="date">/2024</span>
+<p align="center" class="bwv">Christoph Graupner GWV 1101/12<br>
+  Wache auf meine Ehre</p>
+<p class="musiciens">
+Karine Serafin*, Hanna Alane, sopranes<br>
+Catherine Joussellin, Adam Harrata, altos<br>
+Jean-François Novelli*, Stanislas Herbin, ténors<br>
+Jean-François Gay*, Raphaël Bleibtreu, Paul Willenbrock, basses
+</p>
+<p class="musiciens">
+Joseba Berrocal, Amadeo Castille, hautbois
+</p>
+<p class="musiciens">
+Ruth Weber**,Sandrine Dupé,
+Guya Martinini, Freddy Eichelberger, violons<br>
+Aik Shin Tan, Anne Solano, altos<br>
+Claire Giardelli, violoncelle<br>
+Valérie Bienvenu, contrebasse<br>
+</p>
+<p class="musiciens">
+Maïna Guillamet, clavecin<br>
+Alonso Torres orgue
+</p>
+<p class="musiciens">
+Sébastien Cadet, NN, souffleurs
+<p class="musiciens">
+* soliste<br>
+** coordination
+</p>
+<p class="musiciens"><span class="pieceorgue"><a href="#HautPage"><img src="./Images/fleche_haut.gif" alt="Retour haut de page"  border="0" class="flechehaut" /></a></span>
+<p><span class="mois">&nbsp;<!--Programme du concert :--></span> <!--<a href="../Presentations/saison27/2025_12.pdf" target="_blank"><img src="../Images/Acrobat30px.gif" width="30" height="30" border="0"></a>--> </p>
+<hr noshade="noshade" />
+
 </div>
