980b314b2abd9eb1c10b1a4c8f2b4383440038fb
Kai Carver <kaicarver@gmail.com>
2025-12-10 10:34:50 +0100

    vire DOCTYPE en trop

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link rel="stylesheet" type="text/css" href="docs/style.css" />
-<title>Les Cantates.org</title>
-<style type="text/css">
-img        { vertical-align:middle; border:0px }
-</style>
-<script language="JavaScript" type="text/JavaScript">
-<!--
-function MM_reloadPage(init) {  //reloads the window if Nav4 resized
-  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
-    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
-  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
-}
-MM_reloadPage(true);
-//-->
-</script>
-</head>
-<body>
-<div id="Layer1" style="width: 785px; margin-left: auto; margin-right: auto;">
-  <?php
-
+<?php
 $pages = array('accueil' => 'docs/accueil.php',
 			   'suite' => 'docs/suite.php',
 			   'inscription_presence' => 'docs/inscription_presence.php',
@@ -77,9 +54,7 @@ include("docs/header.php");
 include($page_include);
 include("docs/footer.php");
 ?>
-</div>
 
-</body>	
 
 
 
