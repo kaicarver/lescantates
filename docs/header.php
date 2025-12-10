d7305f92da2f77e2c9b5232bb050cb9981fa65c7
Kai Carver <kaicarver@gmail.com>
2025-12-10 10:18:20 +0100

    cantate en cours : Janvier

--- docs/header.php
+++ docs/header.php
@@ -3,9 +3,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <link rel="stylesheet" type="text/css" href="docs/style.css" />
-<title>Les Cantates.org</title>
-	<style type="text/css">
-img        { vertical-align:middle; border:0px }
+<title>
+	Les Cantates 
+	<?php if ($_GET['page'] != NULL AND $_GET['page'] != "accueil" ) { echo " - ", $_GET['page']; } ?>
+</title>
+
+<style type="text/css">
+img { vertical-align:middle; border:0px }
 </style>
 </head>
 <body>
@@ -18,8 +22,8 @@ echo <<<EOF
 <div align="right"><a href="index.php?page=suite"><img src="../Images/Logo_Lescantates_Lucida_large.gif" alt="logo"></a></div>
 <br />
 <div class="cellulemenu"><a href="index.php?page=nextcantate">La prochaine cantate&nbsp;</a> 
-<a href="index.php?page=integrale25">Les cantates en cours&nbsp;</a>
-<a href="index.php?page=accueilorgue">le nouvel orgue&nbsp;</a>
+<a href="index.php?page=integrale27#Janvier">Les cantates en cours&nbsp;</a>
+<a href="index.php?page=accueilorgue">Le nouvel orgue&nbsp;</a>
 <a href="index.php?page=contacts">Contact|Liens&nbsp;</a>
 </div>
 EOF;
