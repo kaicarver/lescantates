d583d85bfcd7681e6b3bb0d57991247bda0585dc
Kai Carver <kaicarver@gmail.com>
2025-12-11 14:38:13 +0100

    version brouillon du PDF de décembre

--- docs/integrale27.php
+++ docs/integrale27.php
@@ -124,7 +124,7 @@ Sébastien Cadet, NN, souffleurs
 ** coordination
 </p>
 <p class="musiciens"><span class="pieceorgue"><a href="#HautPage"><img src="./Images/fleche_haut.gif" alt="Retour haut de page"  border="0" class="flechehaut" /></a></span>
-<p><span class="mois">&nbsp;<!--Programme du concert :--></span> <!--<a href="../Presentations/saison27/2025_12.pdf" target="_blank"><img src="../Images/Acrobat30px.gif" width="30" height="30" border="0"></a>--> </p>
+<p><span class="mois">&nbsp;Programme du concert :</span> (brouillon temporaire) <a href="../Presentations/saison27/2025_12.pdf" target="_blank"><img src="../Images/Acrobat30px.gif" width="30" height="30" border="0"></a> </p>
 <hr noshade="noshade" />
 
 <p class="mois"><a name="Janvier" id="Janvier"></a>JANVIER<br>
@@ -137,7 +137,7 @@ Sébastien Cadet, NN, souffleurs
 &nbsp;
 </p>
 <p class="musiciens"><span class="pieceorgue"><a href="#HautPage"><img src="./Images/fleche_haut.gif" alt="Retour haut de page"  border="0" class="flechehaut" /></a></span>
-<p><span class="mois">&nbsp;<!--Programme du concert :--></span> <!--<a href="../Presentations/saison27/2025_12.pdf" target="_blank"><img src="../Images/Acrobat30px.gif" width="30" height="30" border="0"></a>--> </p>
+<p><span class="mois">&nbsp;<!--Programme du concert :--></span> <!--<a href="../Presentations/saison27/2026_01.pdf" target="_blank"><img src="../Images/Acrobat30px.gif" width="30" height="30" border="0"></a>--> </p>
 <hr noshade="noshade" />
 
 </div>
