8ec8245ab212cb7de906ae9b3f111d4407dddffe
Kai Carver <kaicarver@gmail.com>
2026-05-09 14:14:09 +0200

    mise a jour page prochaine cantate pour juin

--- docs/nextcantate.php
+++ docs/nextcantate.php
@@ -12,23 +12,20 @@
       <p>
 	<strong>
 	  <em>Prochaine cantate</em><br>
-	  Dimanche 3 mai 2026 à 17h30
+	  Dimanche 7 juin 2026 à 17h30
         </strong>
       </p>
       <div>
-	Cantate BWV 103 Ihr werdet weinen und heulen<br>
-	BWV 528 Sonate en trio pour Flûte et Orgue
+        BWV 39 <em>Brich dem Hungrigen dein Brot</em>
       </div>
       <div>&nbsp;</div>
       <div>
-        coordination : Elisabeth Joyé et Sébastien Marq
+        Coordination : Marine Fribourg
       </div>
     </div>
   </div>
   <p>
-La Cantate est écrite pour le 3ème dimanche après Pâques et a été exécutée pour la première fois à Leipzig le 22 avril 1725.<br>
-Son livret est de la poétesse Christiane Marianne von Ziegler, basé sur les paroles du Christ, tirées de l'Évangile selon Saint Jean, annonçant à ses disciples sa mort et sa résurrection :<br>
-"Vous gémirez et vous pleurerez, mais le monde se réjouira".
+    <a href="https://fr.wikipedia.org/wiki/Brich_dem_Hungrigen_dein_Brot" target="_blank">https://fr.wikipedia.org/wiki/Brich_dem_Hungrigen_dein_Brot</a>
   </p>
   <p class="mois">&nbsp;</p>
   
